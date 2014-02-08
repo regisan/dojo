9967f9ec1b820e80f830903f9947bbaa0cf001db
Regis Santos <regisan@gmail.com>
2014-02-08 00:58:55 -0200

    Refactoring dojo 007
    Refactoring dojo 007

--- dojo007/src/main/java/com/tr/dojo007/ContaLetras.java
+++ dojo007/src/main/java/com/tr/dojo007/ContaLetras.java
@@ -4,54 +4,53 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Dojo Contando as letras dos números
+ * Dojo Contando as letras dos numeros
  * @see http://dojopuzzles.com/problemas/exibe/contando-as-letras-dos-numeros/
  *
  */
 public class ContaLetras {
 	
-	private static Map<Integer, String> mapa = new HashMap<>();
+	private static final Map<Integer, String> MAPA = new HashMap<>();
 	private static final int TOTAL_LETRAS_CEM = 3;
 	
 	static {
-		mapa.put(1, "um");
-		mapa.put(2, "dois");
-		mapa.put(3, "tres");
-		mapa.put(4, "quatro");
-		mapa.put(5, "cinco");
-		mapa.put(6, "seis");
-		mapa.put(7, "sete");
-		mapa.put(8, "oito");
-		mapa.put(9, "nove");
-		mapa.put(10, "dez");
-		mapa.put(11, "onze");
-		mapa.put(12, "doze");
-		mapa.put(13, "treze");
-		mapa.put(14, "quatorze");
-		mapa.put(15, "quinze");
-		mapa.put(16, "dezesseis");
-		mapa.put(17, "dezessete");
-		mapa.put(18, "dezoito");
-		mapa.put(19, "dezenove");
-		mapa.put(20, "vinte");
-		mapa.put(30, "trinta");
-		mapa.put(40, "quarenta");
-		mapa.put(50, "cinquenta");
-		mapa.put(60, "sessenta");
-		mapa.put(70, "setenta");
-		mapa.put(80, "oitenta");
-		mapa.put(90, "noventa");
-		mapa.put(100, "cento");
-		mapa.put(200, "duzentos");
-		mapa.put(300, "trezentos");
-		mapa.put(400, "quatrocentos");
-		mapa.put(500, "quinhentos");
-		mapa.put(600, "seiscentos");
-		mapa.put(700, "setecentos");
-		mapa.put(800, "oitocentos");
-		mapa.put(900, "novecentos");
-		mapa.put(1000, "mil");
-		
+		MAPA.put(1, "um");
+		MAPA.put(2, "dois");
+		MAPA.put(3, "tres");
+		MAPA.put(4, "quatro");
+		MAPA.put(5, "cinco");
+		MAPA.put(6, "seis");
+		MAPA.put(7, "sete");
+		MAPA.put(8, "oito");
+		MAPA.put(9, "nove");
+		MAPA.put(10, "dez");
+		MAPA.put(11, "onze");
+		MAPA.put(12, "doze");
+		MAPA.put(13, "treze");
+		MAPA.put(14, "quatorze");
+		MAPA.put(15, "quinze");
+		MAPA.put(16, "dezesseis");
+		MAPA.put(17, "dezessete");
+		MAPA.put(18, "dezoito");
+		MAPA.put(19, "dezenove");
+		MAPA.put(20, "vinte");
+		MAPA.put(30, "trinta");
+		MAPA.put(40, "quarenta");
+		MAPA.put(50, "cinquenta");
+		MAPA.put(60, "sessenta");
+		MAPA.put(70, "setenta");
+		MAPA.put(80, "oitenta");
+		MAPA.put(90, "noventa");
+		MAPA.put(100, "cento");
+		MAPA.put(200, "duzentos");
+		MAPA.put(300, "trezentos");
+		MAPA.put(400, "quatrocentos");
+		MAPA.put(500, "quinhentos");
+		MAPA.put(600, "seiscentos");
+		MAPA.put(700, "setecentos");
+		MAPA.put(800, "oitocentos");
+		MAPA.put(900, "novecentos");
+		MAPA.put(1000, "mil");
 	}
 
 	public int contar(int n) {
@@ -66,30 +65,28 @@ public class ContaLetras {
 	            soma += TOTAL_LETRAS_CEM;
 	        }
 	        else {
-	            soma += mapa.get(valorDecomposto).length();
-	        }
-	        
-	        // decompor a dezena do número i
-	        if (valorDecomposto < i) {
-	            soma++;
-	            int sobra = i - valorDecomposto;
-	            if (sobra > 20) {
-	                valorDecomposto = decompor(sobra);
-	                soma += mapa.get(valorDecomposto).length();
-	                
-	                // ainda tem a unidade
-	                if (sobra > valorDecomposto) {
-	                    soma++;
-	                    soma += mapa.get(sobra % 10).length();
+	            soma += MAPA.get(valorDecomposto).length();
+
+	            // decompor a dezena do numero i
+	            if (valorDecomposto < i) {
+	                soma++; // incrementa com a conjuncao "e" (cento "e" um)
+	                int sobra = i - valorDecomposto;
+	                if (sobra > 20) {
+	                    valorDecomposto = decompor(sobra);
+	                    soma += MAPA.get(valorDecomposto).length();
+	                    
+	                    // ainda tem a unidade
+	                    if (sobra > valorDecomposto) {
+	                        soma++;
+	                        soma += MAPA.get(sobra % 10).length();
+	                    }
 	                }
-	                
-	            }
-	            else {
-	                soma += mapa.get(i - valorDecomposto).length();
+	                else {
+	                    soma += MAPA.get(i - valorDecomposto).length();
+	                }
 	            }
 	        }
 	    }
-
 		
 		return soma;
 	}
@@ -99,7 +96,7 @@ public class ContaLetras {
 	 * Para valor >= 1000, retorna a milhar (ex: 1000, 2000, 3000)
 	 * Para 100 <= valor < 1000, retorna a centena (ex: 100, 200, 300)
 	 * Para 20 <= valor < 100, retorna a dezena (ex: 20, 30, 40)
-	 * Para valor < 20, retorna o próprio valor sem decompor
+	 * Para valor < 20, retorna o proprio valor sem decompor
 	 * @param valor
 	 * @return milhar, centena, dezena ou o próprio valor
 	 */

6eca002c233ecc7aa504ebaf4381f4811df4d5c5
Regis Santos <regisan@gmail.com>
2016-03-24 16:44:17 -0300

    Refactoring e novos testes

--- 2016/dojo2016-02/spec/AppSpec.js
+++ 2016/dojo2016-02/spec/AppSpec.js
@@ -1,4 +1,5 @@
 describe("Caixa Eletronico", function() {
+
     it("should be 10 reais", function() {
         var valor = 10;
         expect(saque(valor)).toEqual([1,0,0,0]);
@@ -22,6 +23,20 @@ describe("Caixa Eletronico", function() {
     it("should be 30 reais", function() {
         var valor = 30;
         expect(saque(valor)).toEqual([1,1,0,0]);
-    })
+    });
 
+    it("should be 880 reais", function() {
+        var valor = 880;
+        expect(saque(valor)).toEqual([1,1,1,8]);
+    });
+
+    it("should be 40 reais", function() {
+        var valor = 40;
+        expect(saque(valor)).toEqual([0,2,0,0]);
+    });
+
+    it("should be 60 reais", function() {
+        var valor = 60;
+        expect(saque(valor)).toEqual([1,0,1,0]);
+    });
 });
--- 2016/dojo2016-02/src/App.js
+++ 2016/dojo2016-02/src/App.js
@@ -1,28 +1,28 @@
 function saque(valor) {
-    var notas = [10, 20, 50, 100];
 
     var nota10 = 0;
     var nota20 = 0;
     var nota50 = 0;
     var nota100 = 0;
-    var valores = [];
+
     while (valor > 0) {
         if (valor >= 100) {
-            nota100 = nota100 + 1;
-            valor = valor - 100
+            nota100++;
+            valor -= 100;
         }
         else if (valor >= 50) {
-            nota50 = nota50 + 1;
-            valor = valor - 50
+            nota50++;
+            valor -= 50;
         }
         else if (valor >= 20) {
-            nota20 = nota20 + 1;
-            valor = valor - 20
+            nota20++;
+            valor -= 20;
         }
         else if (valor >= 10) {
-            nota10 = nota10 + 1;
-            valor = valor - 10
+            nota10++;
+            valor -= 10;
         }
     }
-    return valores = [nota10,nota20,nota50,nota100];
+
+    return [nota10,nota20,nota50,nota100];
 }

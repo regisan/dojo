8d0b7f405521896378f4c61e1d4c160c7be930fc
Regis Santos <regisan@gmail.com>
2016-03-24 22:09:43 -0300

    Refactoring para limitar dinheiro disponivel do caixa

--- 2016/dojo2016-02/spec/AppSpec.js
+++ 2016/dojo2016-02/spec/AppSpec.js
@@ -1,42 +1,140 @@
-describe("Caixa Eletronico", function() {
+describe("Caixa Eletronico", function(){
+    it("should be 10 reais", function(){
+        expect(sacar(10)).toEqual([1,0,0,0]);
+    });
+
+    it("should throw an error because it does not have enough money", function() {
+        expect(function() {sacar(5000);}).toThrow(new Error("Caixa sem dinheiro suficiente"));
+    });
+
+    it("should be 20 reais", function() {
+        expect(sacar(20)).toEqual([0,1,0,0]);
+    });
+
+    it("should be 50 reais", function() {
+        expect(sacar(50)).toEqual([0,0,1,0]);
+    });
+
+    it("should be 100 reais", function() {
+        expect(sacar(100)).toEqual([0,0,0,1]);
+    });
+
+    it("should be 30 reais", function() {
+        expect(sacar(30)).toEqual([1,1,0,0]);
+    });
+
+    it("should be 880 reais", function() {
+        expect(sacar(880)).toEqual([1,1,1,8]);
+    });
+
+    it("should be 40 reais", function() {
+        expect(sacar(40)).toEqual([0,2,0,0]);
+    });
+
+    it("should be 60 reais", function() {
+        expect(sacar(60)).toEqual([1,0,1,0]);
+    });
+
+    it("should be 1100 reais (10 x R$ 100 and 2 x R$ 50)", function() {
+        expect(sacar(1100)).toEqual([0,0,2,10]);
+    });
+
+    it("should be 1600 reais (10 x R$ 100 and 10 x R$ 50 and 5 x R$ 20)", function() {
+        expect(sacar(1600)).toEqual([0,5,10,10]);
+    });
+
+    it("should be 1710 reais (10 x R$ 100 and 10 x R$ 50 and 10 x R$ 20 and 1 x R$ 10)", function() {
+        expect(sacar(1710)).toEqual([1,10,10,10]);
+    });
+
+    it("should be limpar o caixa", function() {
+        expect(sacar(1800)).toEqual([10,10,10,10]);
+    });
+});
+
+describe("Calcular notas do saque", function() {
+
+    var notasRetornadas;
+    var cx;
+
+    beforeEach(function() {
+        notasRetornadas = [0, 0, 0, 0];
+        cx = new Caixa(10,10,10,10);
+    })
 
     it("should be 10 reais", function() {
         var valor = 10;
-        expect(saque(valor)).toEqual([1,0,0,0]);
+        expect(calcular(cx, valor, notasRetornadas, NOTAS.length - 1)).toEqual([1,0,0,0]);
     });
 
     it("should be 20 reais", function() {
         var valor = 20;
-        expect(saque(valor)).toEqual([0,1,0,0]);
+        expect(calcular(cx, valor, notasRetornadas, NOTAS.length - 1)).toEqual([0,1,0,0]);
     });
 
     it("should be 50 reais", function() {
         var valor = 50;
-        expect(saque(valor)).toEqual([0,0,1,0]);
+        expect(calcular(cx, valor, notasRetornadas, NOTAS.length - 1)).toEqual([0,0,1,0]);
     });
 
     it("should be 100 reais", function() {
         var valor = 100;
-        expect(saque(valor)).toEqual([0,0,0,1]);
+        expect(calcular(cx, valor, notasRetornadas, NOTAS.length - 1)).toEqual([0,0,0,1]);
     });
 
     it("should be 30 reais", function() {
         var valor = 30;
-        expect(saque(valor)).toEqual([1,1,0,0]);
+        expect(calcular(cx, valor, notasRetornadas, NOTAS.length - 1)).toEqual([1,1,0,0]);
     });
 
     it("should be 880 reais", function() {
         var valor = 880;
-        expect(saque(valor)).toEqual([1,1,1,8]);
+        expect(calcular(cx, valor, notasRetornadas, NOTAS.length - 1)).toEqual([1,1,1,8]);
     });
 
     it("should be 40 reais", function() {
         var valor = 40;
-        expect(saque(valor)).toEqual([0,2,0,0]);
+        expect(calcular(cx, valor, notasRetornadas, NOTAS.length - 1)).toEqual([0,2,0,0]);
     });
 
     it("should be 60 reais", function() {
         var valor = 60;
-        expect(saque(valor)).toEqual([1,0,1,0]);
+        expect(calcular(cx, valor, notasRetornadas, NOTAS.length - 1)).toEqual([1,0,1,0]);
+    });
+});
+
+describe("Manipular cofre do caixa", function() {
+
+    it("should is empty", function() {
+        var c = new Caixa(0,0,0,0);
+        expect(c.isEmpty()).toBeTruthy();
+    });
+
+    it("should not empty", function() {
+        var c = new Caixa(1,0,0,0);
+        expect(c.isEmpty()).toBeFalsy();
+    });
+
+    it("should be 150 no total", function() {
+        var c = new Caixa(0,0,1,1);
+        expect(c.getTotal()).toEqual(150);
+    });
+
+    it("should be 50 depois de sacar 100", function() {
+        var c = new Caixa(0,0,1,1); // 150
+        c.getMoney(3, 1);
+        expect(c.getTotal()).toEqual(50);
+    });
+
+    it("should be 100 de saldo", function() {
+        var c = new Caixa(0,0,1,1); // 150
+        expect(c.getMoney(3, 5)).toEqual(-4);
+
+    });
+
+    it("should be -1 de saldo", function() {
+        var c = new Caixa(0,0,0,10); // 1000
+        expect(c.getMoney(3, 11)).toEqual(-1);
+
     });
 });
--- 2016/dojo2016-02/src/App.js
+++ 2016/dojo2016-02/src/App.js
@@ -1,28 +1,73 @@
-function saque(valor) {
+var NOTAS = [10, 20, 50, 100];
 
-    var nota10 = 0;
-    var nota20 = 0;
-    var nota50 = 0;
-    var nota100 = 0;
+function Caixa(qtd10, qtd20, qtd50, qtd100) {
 
-    while (valor > 0) {
-        if (valor >= 100) {
-            nota100++;
-            valor -= 100;
+    this.cofre = [qtd10, qtd20, qtd50, qtd100];
+
+    this.isEmpty = function() {
+        for (var i = 0; i < this.cofre.length; i++)
+            if (this.cofre[i] !== 0)
+                return false;
+
+        return true;
+    };
+
+    this.getTotal = function() {
+        var sum = 0;
+        for (var i = 0; i < this.cofre.length; i++) {
+            sum += this.cofre[i] * NOTAS[i];
         }
-        else if (valor >= 50) {
-            nota50++;
-            valor -= 50;
-        }
-        else if (valor >= 20) {
-            nota20++;
-            valor -= 20;
-        }
-        else if (valor >= 10) {
-            nota10++;
-            valor -= 10;
-        }
+
+        return sum;
     }
 
-    return [nota10,nota20,nota50,nota100];
-}
+    this.getMoney = function(idx, qtde) {
+        if (this.cofre[idx] >= qtde) {
+            return this.cofre[idx] -= qtde;
+        }
+        else {
+            // nao ha notas suficientes
+            var disponivel = this.cofre[idx];
+            this.cofre[idx] = 0;
+            return disponivel - qtde;
+        }
+    }
+};
+
+function sacar(valor) {
+    var cx = new Caixa(10,10,10,10); // total do caixa = R$ 1800
+    return calcular(cx, valor, [0, 0, 0, 0], NOTAS.length - 1);
+};
+
+function calcular(cx, valor, notasRetornadas, idx) {
+
+    if (cx.isEmpty() && valor > 0)
+        throw new Error("Caixa sem dinheiro");
+
+    if (valor > cx.getTotal())
+        throw new Error("Caixa sem dinheiro suficiente");
+
+    if (valor == 0) {
+        return notasRetornadas;
+    }
+    else {
+        var qtdeNotasNecessarias = parseInt(valor / NOTAS[idx]);
+
+        if (qtdeNotasNecessarias >= 1) {
+            var saldo = cx.getMoney(idx, qtdeNotasNecessarias);
+
+            if (saldo >= 0) {
+                notasRetornadas[idx] = qtdeNotasNecessarias;
+            }
+            else {
+                notasRetornadas[idx] = qtdeNotasNecessarias + saldo;
+                var resto = valor % NOTAS[idx];
+                var vlrPendente = (NOTAS[idx] * saldo * - 1) + resto;
+                return calcular(cx, vlrPendente, notasRetornadas, --idx);
+            }
+        }
+
+        return calcular(cx, parseInt(valor % NOTAS[idx]), notasRetornadas, --idx);
+    }
+};
+
